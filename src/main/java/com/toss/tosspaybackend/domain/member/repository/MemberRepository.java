91dc3bf90c811ad44e33105144e12062ecc7cfc7
SNOW <kancth03@gmail.com>
2024-02-06 06:24:17 +0900

    Feat: 회원가입 중복검사 쿼리 메서드 등록

--- src/main/java/com/toss/tosspaybackend/domain/member/repository/MemberRepository.java
+++ src/main/java/com/toss/tosspaybackend/domain/member/repository/MemberRepository.java
@@ -4,6 +4,9 @@ import com.toss.tosspaybackend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByNameAndPhoneAndResidentRegistrationNumberFront(String name, String phone, String frontRRN);
 }
