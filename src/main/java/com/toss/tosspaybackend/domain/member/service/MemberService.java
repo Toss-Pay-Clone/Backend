6022dea501661390dded9f35fcea8993268a7063
SNOW <kancth03@gmail.com>
2024-02-06 04:56:50 +0900

    Feat: 주민번호 검증 기능에 성별 검증 추가

--- src/main/java/com/toss/tosspaybackend/domain/member/service/MemberService.java
+++ src/main/java/com/toss/tosspaybackend/domain/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.toss.tosspaybackend.domain.member.service;
 import com.toss.tosspaybackend.domain.member.dto.RegisterRequest;
 import com.toss.tosspaybackend.domain.member.dto.RegisterResponse;
 import com.toss.tosspaybackend.domain.member.entity.Member;
+import com.toss.tosspaybackend.domain.member.enums.Gender;
 import com.toss.tosspaybackend.domain.member.repository.MemberRepository;
 import com.toss.tosspaybackend.global.Response;
 import com.toss.tosspaybackend.global.exception.ErrorCode;
@@ -20,7 +21,9 @@ public class MemberService {
     public Response<RegisterResponse> register(RegisterRequest request) {
 
         validatePhoneNumber(request.phone());
-        validateRRN(request.residentRegistrationNumberFront(), request.residentRegistrationNumberBack());
+        validateRRN(request.residentRegistrationNumberFront(),
+                request.residentRegistrationNumberBack(),
+                request.gender());
         // TODO: 생일 검사 (주민번호 검사 포함)
         // TODO: 중복 검사
 
@@ -47,10 +50,21 @@ public class MemberService {
         }
     }
 
-    private void validateRRN(String frontRRN, String backRRN) {
+    private void validateRRN(String frontRRN, String backRRN, Gender gender) {
+        // 주민번호 앞자리 및 뒷자리 첫번째 검사
         if (!frontRRN.matches("^\\d{2}(0[1-9]|1[0-2])(0[1-9]|[1-2]\\d|3[0-1])$") ||
                 !backRRN.matches("^[0-4|9]$")) {
             throw new GlobalException(ErrorCode.BAD_REQUEST, "주민번호 형식이 유효하지 않습니다.");
         }
+
+        // 주민번호 - 성별 검사
+        if ((gender == Gender.FEMALE && Integer.parseInt(backRRN) % 2 != 0) ||
+                gender == Gender.MALE && Integer.parseInt(backRRN) % 2 != 1) {
+            throw new GlobalException(ErrorCode.BAD_REQUEST, "주민번호와 성별이 일치하지 않습니다.");
+        } else if () {
+            throw new GlobalException(ErrorCode.BAD_REQUEST, "주민번호와 성별이 일치하지 않습니다.");
+        }
     }
+
+
 }
