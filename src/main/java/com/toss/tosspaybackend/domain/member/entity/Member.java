de4741d899cbe27ecf667129f0e4e6263e8c0605
SNOW <kancth03@gmail.com>
2024-02-06 00:51:38 +0900

    fix: 생성시간, 수정시간, 삭제시간 클래스 분리

--- src/main/java/com/toss/tosspaybackend/domain/member/entity/Member.java
+++ src/main/java/com/toss/tosspaybackend/domain/member/entity/Member.java
@@ -3,17 +3,20 @@ package com.toss.tosspaybackend.domain.member.entity;
 import com.toss.tosspaybackend.domain.member.enums.Gender;
 import com.toss.tosspaybackend.domain.member.enums.MobileCarrier;
 import com.toss.tosspaybackend.domain.member.enums.Nationality;
+import com.toss.tosspaybackend.global.basetime.Auditable;
+import com.toss.tosspaybackend.global.basetime.BaseTime;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member implements Auditable {
     @Id
     @GeneratedValue
     private Long id;
@@ -33,7 +36,8 @@ public class Member {
     private MobileCarrier mobileCarrier;
     private LocalDateTime birthdate;
 
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private LocalDateTime deletedAt;
+    @Setter
+    @Embedded
+    @Column(nullable = false)
+    private BaseTime baseTime;
 }
