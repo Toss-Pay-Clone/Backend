c42c3e5c03d210821440ee91e03685343f6ef830
SNOW <kancth03@gmail.com>
2024-02-06 01:01:23 +0900

    feat: 생성자 및 팩토리 메서드 구현

--- src/main/java/com/toss/tosspaybackend/domain/member/entity/Member.java
+++ src/main/java/com/toss/tosspaybackend/domain/member/entity/Member.java
@@ -6,10 +6,7 @@ import com.toss.tosspaybackend.domain.member.enums.Nationality;
 import com.toss.tosspaybackend.global.basetime.Auditable;
 import com.toss.tosspaybackend.global.basetime.BaseTime;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -40,4 +37,33 @@ public class Member implements Auditable {
     @Embedded
     @Column(nullable = false)
     private BaseTime baseTime;
+
+    @Builder
+    private Member(String name, String phone, Gender gender, Nationality nationality,
+                  int firstResidentRegistrationNumber, int secondResidentRegistrationNumber,
+                  MobileCarrier mobileCarrier, LocalDateTime birthdate) {
+        this.name = name;
+        this.phone = phone;
+        this.gender = gender;
+        this.nationality = nationality;
+        this.firstResidentRegistrationNumber = firstResidentRegistrationNumber;
+        this.secondResidentRegistrationNumber = secondResidentRegistrationNumber;
+        this.mobileCarrier = mobileCarrier;
+        this.birthdate = birthdate;
+    }
+
+    public static Member of(String name, String phone, Gender gender, Nationality nationality,
+                            int firstResidentRegistrationNumber, int secondResidentRegistrationNumber,
+                            MobileCarrier mobileCarrier, LocalDateTime birthdate) {
+        return Member.builder()
+                .name(name)
+                .phone(phone)
+                .gender(gender)
+                .nationality(nationality)
+                .firstResidentRegistrationNumber(firstResidentRegistrationNumber)
+                .secondResidentRegistrationNumber(secondResidentRegistrationNumber)
+                .mobileCarrier(mobileCarrier)
+                .birthdate(birthdate)
+                .build();
+    }
 }
