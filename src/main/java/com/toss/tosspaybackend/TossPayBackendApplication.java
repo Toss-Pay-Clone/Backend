9e0b3738dc8231c8c222191a1db0f9963a2009ff
TaeHee00 <kancth03@gmail.com>
2024-02-05 16:25:07 +0900

    프로젝트 생성

---
+++ src/main/java/com/toss/tosspaybackend/TossPayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.toss.tosspaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossPayBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TossPayBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toss/tosspaybackend/TossPayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toss.tosspaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossPayBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
