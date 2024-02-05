07fd0602b8a38a143ee354203417ece6003ee025
SNOW <kancth03@gmail.com>
2024-02-06 01:13:04 +0900

    Feat: MemberRepository 구현

---
+++ src/main/java/com/toss/tosspaybackend/domain/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.toss.tosspaybackend.domain.member.repository;
+
+import com.toss.tosspaybackend.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
