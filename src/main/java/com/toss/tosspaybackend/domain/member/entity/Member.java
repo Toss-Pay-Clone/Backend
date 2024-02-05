2a490bf1c4b0cb227e304070e322d81a932f64f7
SNOW <kancth03@gmail.com>
2024-02-06 00:35:04 +0900

    refactor: import 구문 간결하게 정리

--- src/main/java/com/toss/tosspaybackend/domain/member/entity/Member.java
+++ src/main/java/com/toss/tosspaybackend/domain/member/entity/Member.java
@@ -1,8 +1,6 @@
 package com.toss.tosspaybackend.domain.member.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
