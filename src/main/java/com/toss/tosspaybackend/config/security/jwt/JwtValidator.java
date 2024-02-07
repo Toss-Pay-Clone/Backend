281cb4f5421f6b0668625a5f8c1aec6c56809850
SNOW <kancth03@gmail.com>
2024-02-07 12:35:14 +0900

    Feat: Refresh Token도 포함하여 검증 로직 구현

--- src/main/java/com/toss/tosspaybackend/config/security/jwt/JwtValidator.java
+++ src/main/java/com/toss/tosspaybackend/config/security/jwt/JwtValidator.java
@@ -2,12 +2,10 @@ package com.toss.tosspaybackend.config.security.jwt;
 
 import com.toss.tosspaybackend.domain.member.entity.Member;
 import com.toss.tosspaybackend.domain.member.repository.MemberRepository;
-import com.toss.tosspaybackend.global.exception.ErrorCode;
-import com.toss.tosspaybackend.global.exception.CustomAccessDeniedHandler;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -19,34 +17,58 @@ import java.security.Key;
 import java.util.ArrayList;
 import java.util.Collection;
 
-@Component
+@Slf4j
 @RequiredArgsConstructor
+@Component
 public class JwtValidator {
     private final Key key;
     private final MemberRepository memberRepository;
 
-    public Authentication getAuthentication(String accessToken) {
+    public Authentication getAuthentication(String accessToken, String refreshToken) {
         Collection<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
 
+        // Refresh Token Validation
+        try {
+            Claims claims = getTokenBodyClaims(refreshToken);
+            // ? 이건 남겨야하나? 검사하면 좋을꺼같긴 한데 쿼리문을 한번 더 쓰는 오버헤드가 있네
+            // TODO: 만약 쓸꺼면 exists 쿼리로 사용
+            // TODO: 만료 시간 검사 후 얼마 안남았으면 재발급
+            Member member = memberRepository.findById(claims.get("id", Long.class))
+                    .orElseThrow(() -> new AccessDeniedException("Invalid Token"));
+        } catch (JwtException e) {
+            // TODO: 재발급 로직이 들어갈 수 도 있음
+            getTokenStatus(e, TokenType.REFRESH_TOKEN);
+        } catch (Exception e) {
+            log.error("JWT Exception", e);
+        }
+
+        // Access Token Validation
         try {
             Claims claims = getTokenBodyClaims(accessToken);
             Member member = memberRepository.findById(claims.get("id", Long.class))
                     .orElseThrow(() -> new AccessDeniedException("Invalid Token"));
 
             return new UsernamePasswordAuthenticationToken(member, "", authorities);
-        } catch (ExpiredJwtException e) {
-            throw new AccessDeniedException("Token expired");
+
+        } catch (JwtException e) {
+            // TODO: 재발급 로직이 들어가야하기 때문에 반환값 필요
+            getTokenStatus(e, TokenType.ACCESS_TOKEN);
+        } catch (Exception e) {
+            log.error("JWT Exception", e);
         }
 
-
+        return null;
     }
 
-    private Claims getTokenBodyClaims(String accessToken) {
+    private Claims getTokenBodyClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()
-                .parseClaimsJws(accessToken)
+                .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private TokenStatus getTokenStatus(JwtException e, TokenType tokenType) {
     }
 }
