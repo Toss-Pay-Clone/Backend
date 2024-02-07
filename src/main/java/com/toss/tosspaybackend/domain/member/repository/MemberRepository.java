6db41741ea62f06e89361e9c26c951a818e7a708
SNOW <kancth03@gmail.com>
2024-02-07 20:07:18 +0900

    Feat: 가입 후 탈퇴한 회원을 찾기 위한 Native Query 구현

--- src/main/java/com/toss/tosspaybackend/domain/member/repository/MemberRepository.java
+++ src/main/java/com/toss/tosspaybackend/domain/member/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.toss.tosspaybackend.domain.member.repository;
 
 import com.toss.tosspaybackend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +12,7 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByNameAndPhoneAndResidentRegistrationNumberFront(String name, String phone, String frontRRN);
     Optional<Member> findByPhone(String phone);
+    @Query(value = "SELECT * FROM member m WHERE m.phone = :phone AND m.deleted_at IS NOT NULL", nativeQuery = true)
+    Optional<Member> findByPhoneAndDeleted(@Param("phone") String phone);
     boolean existsByPhone(String phone);
 }
