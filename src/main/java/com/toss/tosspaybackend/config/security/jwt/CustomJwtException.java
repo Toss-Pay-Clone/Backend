a1208d7c6046104d60d75eb056a93b447edb880d
SNOW <kancth03@gmail.com>
2024-02-07 14:53:34 +0900

    Feat: JWT Exception에 TokenType을 명시하기 위해 Custom Exception 구현

---
+++ src/main/java/com/toss/tosspaybackend/config/security/jwt/CustomJwtException.java
@@ -0,0 +1,25 @@
+package com.toss.tosspaybackend.config.security.jwt;
+
+import com.toss.tosspaybackend.config.security.jwt.enums.TokenType;
+import io.jsonwebtoken.JwtException;
+import lombok.Getter;
+
+@Getter
+public class CustomJwtException extends JwtException {
+    private final TokenType tokenType;
+
+    public CustomJwtException(TokenType tokenType, String message) {
+        super(message);
+        this.tokenType = tokenType;
+    }
+
+    public CustomJwtException(TokenType tokenType, Throwable cause) {
+        super(cause.getMessage(), cause);
+        this.tokenType = tokenType;
+    }
+
+    public CustomJwtException(TokenType tokenType, String message, Throwable cause) {
+        super(message, cause);
+        this.tokenType = tokenType;
+    }
+}
