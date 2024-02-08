dd59c72c77ef84e1b178b70ef03aa66f2732e8fe
SNOW <kancth03@gmail.com>
2024-02-09 05:43:14 +0900

    Feat: Redis의 설정을 위한 Properties 클래스 구현

--- src/main/java/com/toss/tosspaybackend/config/redis/RedisProperties.java
+++ src/main/java/com/toss/tosspaybackend/config/redis/RedisProperties.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Component;
 
 @Setter
 @Getter
-@ConfigurationProperties(prefix = "spring.redis")
+@ConfigurationProperties(prefix = "spring.data.redis")
 @Component
 public class RedisProperties {
+    private String host;
+    private int port;
 }
