c7ab6d89e491a659284c4c50d5b27fcbe3d626cd
SNOW <kancth03@gmail.com>
2024-02-11 05:30:41 +0900

    Feat: 계정의 상태를 나타내기 위한 Enum클래스 구현

---
+++ src/main/java/com/toss/tosspaybackend/domain/member/enums/AccountStatus.java
@@ -0,0 +1,8 @@
+package com.toss.tosspaybackend.domain.member.enums;
+
+public enum AccountStatus {
+    // ACTIVE - 정상 계정
+    // BANNED - 부정 행위 또는 관리자에 의해 정지된 계정
+    // SUSPENDED - 일시정지된 계정
+    ACTIVE, BANNED, SUSPENDED
+}
