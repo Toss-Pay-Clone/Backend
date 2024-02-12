c4da434c3460fab41cfe98ff660053696138d92c
SNOW <kancth03@gmail.com>
2024-02-12 12:02:56 +0900

    Refactor: JWT Filter 사용하지 않는 import삭제

--- src/main/java/com/toss/tosspaybackend/config/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/toss/tosspaybackend/config/security/jwt/JwtAuthenticationFilter.java
@@ -11,7 +11,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
