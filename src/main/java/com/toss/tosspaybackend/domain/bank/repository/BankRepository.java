4d6eb6292e49c4bf8edf1600851b36bf96a70393
SNOW <kancth03@gmail.com>
2024-02-13 00:31:31 +0900

    Feat: BankAccountRepository 구현

--- src/main/java/com/toss/tosspaybackend/domain/bank/repository/BankRepository.java
+++
@@ -1,7 +0,0 @@
-package com.toss.tosspaybackend.domain.bank.repository;
-
-import com.toss.tosspaybackend.domain.bank.entity.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankRepository extends JpaRepository<Bank, Long> {
-}
