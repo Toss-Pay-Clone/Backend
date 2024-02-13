955aaf8066c080e21306e23965384c581c29ff90
SNOW <kancth03@gmail.com>
2024-02-14 07:02:54 +0900

    Feat: BankAccount Entity에 Member Entity 연결

--- src/main/java/com/toss/tosspaybackend/domain/bank/entity/BankAccount.java
+++ src/main/java/com/toss/tosspaybackend/domain/bank/entity/BankAccount.java
@@ -1,6 +1,7 @@
 package com.toss.tosspaybackend.domain.bank.entity;
 
 import com.toss.tosspaybackend.domain.bank.enums.Bank;
+import com.toss.tosspaybackend.domain.member.entity.Member;
 import com.toss.tosspaybackend.global.basetime.AuditListener;
 import com.toss.tosspaybackend.global.basetime.Auditable;
 import com.toss.tosspaybackend.global.basetime.BaseTime;
@@ -23,6 +24,9 @@ public class BankAccount implements Auditable {
 
     @Enumerated(EnumType.STRING)
     private Bank bank;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
     // 계좌 비밀번호
     private String password;
 
