b8a0c6a5fda6c090ee3e6d95503efb0b78c9b7fa
TaeHee00 <kancth03@gmail.com>
2024-02-15 02:27:45 +0900

    Feat: BankAccountRepository 계좌 조회 메서드 등록

--- src/main/java/com/toss/tosspaybackend/domain/bank/repository/BankAccountRepository.java
+++ src/main/java/com/toss/tosspaybackend/domain/bank/repository/BankAccountRepository.java
@@ -5,7 +5,10 @@ import com.toss.tosspaybackend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
     List<BankAccount> findAllByMember(Member member);
+
+    Optional<BankAccount> findByMemberAndBankAccountNumber(Member member, Long bankAccountNumber);
 }
