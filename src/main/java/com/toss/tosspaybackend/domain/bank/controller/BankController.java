03608b6d9cb1b955de08f85c1ae50d96dd8b8e79
SNOW <kancth03@gmail.com>
2024-02-14 23:04:45 +0900

    Feat: 계좌 생성 ResponseStatus Annotation등록

--- src/main/java/com/toss/tosspaybackend/domain/bank/controller/BankController.java
+++ src/main/java/com/toss/tosspaybackend/domain/bank/controller/BankController.java
@@ -6,6 +6,7 @@ import com.toss.tosspaybackend.domain.bank.service.BankService;
 import com.toss.tosspaybackend.global.Response;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,7 @@ public class BankController {
     private final BankService bankService;
 
     @PreAuthorize("hasAuthority(@roleService.getRoleUser())")
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/account")
     public Response<AddBankAccountResponse> addBankAccount(@Valid @RequestBody AddBankAccountRequest request) {
         return bankService.addBankAccount(request);
