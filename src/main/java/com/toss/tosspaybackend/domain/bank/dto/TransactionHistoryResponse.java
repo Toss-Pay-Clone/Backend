05252d86fd204d5b1d51750b66cd29c6f4e7aadf
SNOW <kancth03@gmail.com>
2024-02-17 04:46:37 +0900

    Feat: Entity -> DTO로 가독성 좋은 변환을 위해 팩토리 메서드 구현

--- src/main/java/com/toss/tosspaybackend/domain/bank/dto/TransactionHistoryResponse.java
+++ src/main/java/com/toss/tosspaybackend/domain/bank/dto/TransactionHistoryResponse.java
@@ -1,5 +1,7 @@
 package com.toss.tosspaybackend.domain.bank.dto;
 
+import com.toss.tosspaybackend.domain.bank.entity.BankAccount;
+import com.toss.tosspaybackend.domain.bank.entity.BankAccountTransactionHistory;
 import com.toss.tosspaybackend.domain.bank.enums.TransactionType;
 import lombok.Builder;
 
@@ -16,4 +18,37 @@ public record TransactionHistoryResponse(
         LocalDateTime transactionTime,
         Long balanceAfterTransaction
 ) {
+    public static TransactionHistoryResponse fromEntityDeposit(BankAccountTransactionHistory entity) {
+        return TransactionHistoryResponse.builder()
+                .id(entity.getId())
+                .amount(entity.getAmount())
+                .transactionType(entity.getTransactionType())
+                .description(entity.getWithdrawalDestination().getMember().getName())
+                .depositDestination(buildDestination(entity.getDepositDestination()))
+                .withdrawaDestination(buildDestination(entity.getWithdrawalDestination()))
+                .transactionTime(entity.getBaseTime().getCreatedAt())
+                .balanceAfterTransaction(entity.getBalanceAfterTransaction())
+                .build();
+    }
+
+    public static TransactionHistoryResponse fromEntityWithdrawal(BankAccountTransactionHistory entity) {
+        return TransactionHistoryResponse.builder()
+                .id(entity.getId())
+                .amount(entity.getAmount())
+                .transactionType(entity.getTransactionType())
+                .description(entity.getDepositDestination().getMember().getName())
+                .depositDestination(buildDestination(entity.getDepositDestination()))
+                .withdrawaDestination(buildDestination(entity.getWithdrawalDestination()))
+                .transactionTime(entity.getBaseTime().getCreatedAt())
+                .balanceAfterTransaction(entity.getBalanceAfterTransaction())
+                .build();
+    }
+
+    private static Destination buildDestination(BankAccount bankAccount) {
+        return Destination.builder()
+                .bank(bankAccount.getBank())
+                .name(bankAccount.getName())
+                .bankAccountNumber(bankAccount.getBankAccountNumber())
+                .build();
+    }
 }
