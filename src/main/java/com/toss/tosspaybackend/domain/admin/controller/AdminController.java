132e48cc9dbe8894c1b6e5bd856fd41c5711d690
SNOW <kancth03@gmail.com>
2024-02-20 14:38:26 +0900

    Feat: member page에 Data를 반환하도록 구현

--- src/main/java/com/toss/tosspaybackend/domain/admin/controller/AdminController.java
+++ src/main/java/com/toss/tosspaybackend/domain/admin/controller/AdminController.java
@@ -1,12 +1,28 @@
 package com.toss.tosspaybackend.domain.admin.controller;
 
+import com.toss.tosspaybackend.domain.admin.service.AdminService;
+import com.toss.tosspaybackend.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
+@RequestMapping("/admin")
 @Controller
 public class AdminController {
-    @GetMapping("/admin")
+
+    private final AdminService adminService;
+
+    @GetMapping
     public String adminPage() {
-        return "index";
+        return "/index";
+    }
+
+    @GetMapping("/members")
+    public String membersPage(Model model) {
+        model.addAttribute("members", adminService.memberList());
+        return "/pages/members/members.html";
     }
 }
