1410d2e0d3d2ac66cee2e287c773527860253952
SNOW <kancth03@gmail.com>
2024-02-21 21:42:26 +0900

    Feat: Admin Service 구현 및 member list 기능 구현

--- src/main/java/com/toss/tosspaybackend/domain/admin/service/AdminService.java
+++ src/main/java/com/toss/tosspaybackend/domain/admin/service/AdminService.java
@@ -1,10 +1,16 @@
 package com.toss.tosspaybackend.domain.admin.service;
 
+import com.toss.tosspaybackend.domain.admin.dto.MemberList;
+import com.toss.tosspaybackend.domain.bank.entity.BankAccount;
+import com.toss.tosspaybackend.domain.bank.repository.BankAccountRepository;
 import com.toss.tosspaybackend.domain.member.entity.Member;
 import com.toss.tosspaybackend.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -12,8 +18,34 @@ import java.util.List;
 public class AdminService {
 
     private final MemberRepository memberRepository;
+    private final BankAccountRepository bankAccountRepository;
 
-    public List<Member> memberList() {
-        return memberRepository.findAll();
+    public List<MemberList> memberList() {
+        List<Member> members = memberRepository.findAll();
+        List<MemberList> resList = members.stream().map(member -> {
+            LocalDateTime dateTime = member.getBaseTime().getCreatedAt();
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+            String formattedDateTime = dateTime.format(format);
+
+            List<BankAccount> memberAccount = bankAccountRepository.findAllByMember(member);
+            Long totalBalance = 0L;
+            for (BankAccount bankAccount : memberAccount) {
+                totalBalance += bankAccount.getBalance();
+            }
+
+            NumberFormat format_ = NumberFormat.getInstance();
+            String formattedNumber = format_.format(totalBalance);
+
+
+            return MemberList.builder()
+                    .id(member.getId())
+                    .phone(member.getPhone())
+                    .name(member.getName())
+                    .createdAt(formattedDateTime)
+                    .accountStatus(member.getAccountStatus())
+                    .totalBalance(formattedNumber)
+                    .build();
+        }).toList();
+        return resList;
     }
 }
