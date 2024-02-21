b98df8ccdce3886da35fb788abcd9337b332997f
SNOW <kancth03@gmail.com>
2024-02-21 21:41:48 +0900

    Feat: Detail Page 구현

--- src/main/java/com/toss/tosspaybackend/domain/admin/controller/AdminController.java
+++ src/main/java/com/toss/tosspaybackend/domain/admin/controller/AdminController.java
@@ -1,11 +1,11 @@
 package com.toss.tosspaybackend.domain.admin.controller;
 
 import com.toss.tosspaybackend.domain.admin.service.AdminService;
-import com.toss.tosspaybackend.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -22,7 +22,14 @@ public class AdminController {
 
     @GetMapping("/members")
     public String membersPage(Model model) {
+
         model.addAttribute("members", adminService.memberList());
         return "/pages/members/members.html";
+    }
+
+    @GetMapping("/members/{phone}")
+    public String memberDetailPage(@PathVariable("phone") Long phone, Model model) {
+        model.addAttribute("members", adminService.memberList());
+        return "/pages/members/member-detail.html";
     }
 }
